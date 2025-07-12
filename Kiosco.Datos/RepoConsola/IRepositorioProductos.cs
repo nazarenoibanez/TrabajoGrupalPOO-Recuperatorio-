0a7960de4b188dc4020d62c3918f11818e7ff8f0
Mateo <Mateo@DESKTOP-AS832IS>
2025-07-12 18:03:42 -0300

    repositorio para consola mejorado

---
+++ Kiosco.Datos/RepoConsola/IRepositorioProductos.cs
@@ -0,0 +1,19 @@
+﻿using Kiosco.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kiosco.Datos.RepoConsola
+{
+    public interface IRepositorioProductos
+    {
+        void Agregar(Producto producto);
+        void Editar(Producto producto);
+        void Eliminar(string codigo);
+        bool Existe(string codigo);
+        Producto? ObtenerPorCodigo(string codigo);
+        List<Producto> ObtenerTodos();
+    }
+}
