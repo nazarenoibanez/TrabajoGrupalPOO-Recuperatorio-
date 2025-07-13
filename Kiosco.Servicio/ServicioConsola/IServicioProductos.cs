54d569a406d78ffaeaeaed40de8ba99ef30c4699
Mateo <Mateo@DESKTOP-AS832IS>
2025-07-13 19:53:35 -0300

    Servicio para la consola implementado

---
+++ Kiosco.Servicio/ServicioConsola/IServicioProductos.cs
@@ -0,0 +1,19 @@
+﻿using Kiosco.Entidades;
+using Kiosco.Servicio.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kiosco.Servicio.ServicioConsola
+{
+    public interface IServicioProductos
+    {
+        ValidationResultDto Agregar(Producto producto);
+        ValidationResultDto Editar(Producto producto);
+        ValidationResultDto Eliminar(string codigo);
+        Producto? ObtenerProductoPorCodigo(string codigo);
+        List<Producto> ObtenerTodos();
+    }
+}
